27e87c4f5c08d7e08b1bd027ac36ce5e3f068a02
tuan <phamminhtuann475@gmail.com>
2024-01-06 01:26:25 +0700

    add

---
+++ Java-core/src/main/java/com/laptrinhjavaweb/oopnew/CaChuonImpl.java
@@ -0,0 +1,14 @@
+package com.laptrinhjavaweb.oopnew;
+
+public class CaChuonImpl implements CaChuon{
+	
+	@Override
+	public void boi() {
+		System.out.println("cá nào mà chả biết bơi");
+	}
+
+	@Override
+	public void bay() {
+		System.out.println("chim nào mà chả biết bay");
+	}
+}
